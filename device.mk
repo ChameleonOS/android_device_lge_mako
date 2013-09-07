6f9c27af06d6090af47c156144d392c6da7f8c91
Steve Kondik <shade@chemlab.org>
2013-09-07 01:32:33 -0700

    mako: Use CAF media driver
     * Build codecs, update configs
     * Improves WFD performance
    
    Change-Id: I54c310ca8bb6e687a41cda72a17a722f4fe6b264

--- device.mk
+++ device.mk
@@ -259,9 +259,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 	persist.sys.usb.config=mtp
 
-# Enable SELinux
+# Hardware codecs
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.selinux=1
+    qcom.hw.aac.encoder=true
+
+PRODUCT_PACKAGES += \
+    libOmxAacEnc \
+    libOmxAmrEnc \
+    libOmxEvrcEnc \
+    libOmxQcelp13Enc
 
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 
